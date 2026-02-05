69ac66e7df4c19c78b19599ca107b747340eeba3
Shamiul40 <shamiul4040@gmail.com>
2026-02-05 23:30:23 +0600

    layout design fix and navbar edit

--- components/logo.js
+++ components/logo.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import logo from "/public/lws_logo"
+import logo from "/public/lws_logo.png"
 
 export default function Logo() {
   return (
-    <div>
-        <Link href="/">
+    <nav className='py-4 md:py-6 border-b'>
+        <div className='container mx-auto flex items-center justify-between gap-x-6'>
+            <Link href="/">
             <Image 
             src={logo}
             alt='logo'
@@ -14,6 +15,7 @@ export default function Logo() {
             height={100}
             />
         </Link>
-    </div>
+        </div>
+    </nav>
   )
 }
