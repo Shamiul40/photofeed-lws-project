2a145906f22167ce85614a921a25e2227a999f6e
Shamiul40 <shamiul4040@gmail.com>
2026-02-05 23:38:43 +0600

    image resize

--- components/logo.js
+++ components/logo.js
@@ -1,21 +1,16 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from "/public/lws_logo.png"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "/public/lws_logo.png";
 
 export default function Logo() {
   return (
-    <nav className='py-4 md:py-6 border-b'>
-        <div className='container mx-auto flex items-center justify-between gap-x-6'>
-            <Link href="/">
-            <Image 
-            src={logo}
-            alt='logo'
-            width={100}
-            height={100}
-            />
+    <nav className="py-4 md:py-6 border-b">
+      <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <Link href="/">
+          <Image src={logo} alt="logo" width={100} height={100} />
         </Link>
-        </div>
+      </div>
     </nav>
-  )
+  );
 }
