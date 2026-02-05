e41a3908e0fb7f27b160e3af177d6ba9fb7f41f1
Shamiul40 <shamiul4040@gmail.com>
2026-02-05 22:46:46 +0600

    navbar create

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import logo from "/public/lws_logo"
 
-export default function logo() {
+export default function Logo() {
   return (
     <div>
-      
+        <Link href="/">
+            <Image 
+            src={logo}
+            alt='logo'
+            width={100}
+            height={100}
+            />
+        </Link>
     </div>
   )
 }
---
+++ components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './logo'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <Logo></Logo>
+      </div>
+    </div>
+  )
+}
