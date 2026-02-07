f0a9589cf4d2a83201e1ebbbdd46832410ffc8d0
Shamiul40 <shamiul4040@gmail.com>
2026-02-07 23:05:38 +0600

    dictionary create

--- middleware.js
+++ middleware.js
@@ -2,43 +2,39 @@ import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 import { NextResponse } from "next/server";
 
-
 let locales = ["en", "bn"];
 let defaultLocale = "en";
 
-export function getLocale (request) {
+export function getLocale(request) {
+  const acceptLanguage = request.headers.get("accept-language") ?? undefined;
+  const headers = { "accept-language": acceptLanguage };
+  const languages = new Negotiator({ headers }).languages();
 
-    const acceptLanguage = request.headers.get("accept-language") ?? undefined;
-    const headers = {"accept-language" : acceptLanguage};
-    const languages = new Negotiator({headers}).languages()
-
-    return match(languages, locales, defaultLocale)
-
+  return match(languages, locales, defaultLocale);
 }
 
-
-
 export async function middleware(request) {
-    const pathName = request.nextUrl.pathName;
+  const pathName = request.nextUrl.pathName;
 
-    const pathNameIsMissingInLocale = locales.every(locale=>
-        !pathName.startsWith(`/${locale}/`) && 
-        !pathName.startsWith(`/${locale}`)
-    )
+  const pathNameIsMissingInLocale = locales.every(
+    (locale) =>
+      !pathName.startsWith(`/${locale}/`) && !pathName.startsWith(`/${locale}`),
+  );
 
-    if(pathNameIsMissingInLocale) {
-        const locale = getLocale(request)
+  if (pathNameIsMissingInLocale) {
+    const locale = getLocale(request);
 
-        return NextResponse.redirect(new URL(`/${locale}/${pathName}`, request.url))
-    }
-    
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathName}`, request.url),
+    );
+  }
 }
 
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    '/((?!api|assets|.*\\..*|_next).*)',
+    "/((?!api|assets|.*\\..*|_next).*)",
     // Optional: only run on root (/) URL
     // '/'
   ],
-}
+};
