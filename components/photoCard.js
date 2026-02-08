c9b05247a3c7307eac32be4b0c370864f71de0c0
Shamiul40 <shamiul4040@gmail.com>
2026-02-08 21:29:59 +0600

    try to  image debug

--- components/photoCard.js
+++ components/photoCard.js
@@ -5,8 +5,18 @@ import Link from "next/link";
 export default function PhotoCard({photo}) {
   return (
     <div>
+      <h1>shamiul islam</h1>
+      <p className="sr-only">{photo.url}</p> 
+      <p className="sr-only">{photo.id}</p> 
+      <p className="sr-only">{photo.title}</p> 
      <Link href={`photos/${photo.id}`} className="group">
-        <Image src={"https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=700&q=80"} alt={photo.title} width={700} height={700} />
+        <Image  src={"https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=700&q=80"} alt={photo.title} width={700} height={700} />
+
+        {/* Debug: show raw URL and plain <img> to verify fetch */}
+         
+        {/* <div style={{display: 'none'}}>
+          <img src={photo.url} alt={`debug-${photo.title}`} width="200" height="200" />
+        </div>  */}
 
         {/* <!-- title --> */}
         <div className="title-container">
