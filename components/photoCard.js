da4cbc92e385584ddc81a52b12ca5da460f389ea
Shamiul40 <shamiul4040@gmail.com>
2026-02-09 20:46:24 +0600

    add

--- components/photoCard.js
+++ components/photoCard.js
@@ -13,11 +13,6 @@ export default function PhotoCard({photo}) {
      <Link href={`photos/${photo.id}`} className="group">
         <Image  src={"https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=700&q=80"} alt={photo.title} width={700} height={700} />
 
-        {/* Debug: show raw URL and plain <img> to verify fetch */}
-         
-        {/* <div style={{display: 'none'}}>
-          <img src={photo.url} alt={`debug-${photo.title}`} width="200" height="200" />
-        </div>  */}
 
         {/* <!-- title --> */}
         <div className="title-container">
