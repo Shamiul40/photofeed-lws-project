0db888f8e322c03f97c005cd11771844e2389e5c
Shamiul40 <shamiul4040@gmail.com>
2026-02-11 00:12:37 +0600

    image bug find

--- components/PhotoDetails.js
+++ components/PhotoDetails.js
@@ -4,88 +4,104 @@ import Image from "next/image"
 
 export default async function PhotoDetails({id, lang}) {
 
-    const response = await fetch(`${process.env.BASE_API_URL}/photos/${id}`)
+    // const response = await fetch(`${process.env.BASE_API_URL}/photos/${id}`)
+    const response = await fetch(`${process.env.BASE_API_URL}/photos/${id}`);
     const photo = await response.json(response)
 
-    const dictionary = getDictionary(lang)
+    const dictionary = await getDictionary(lang)
 
   return (
-    <div className="p-6 border rounded-xl col-span-12 lg:col-span-4  ">
+     <div className="grid grid-cols-12 gap-4 2xl:gap-10 ">
+      <div className="col-span-12 lg:col-span-8 border rounded-xl">
+        <Image
+          className="max-w-full h-full max-h-[70vh] mx-auto"
+          src={photo.url}
+          alt={photo.title}
+          width={900}
+          height={500}
+        />
+      </div>
+
+      <div className="p-6 border rounded-xl col-span-12 lg:col-span-4">
         <h2 className="text-lg lg:text-2xl font-bold mb-2">
-          The Beautiful Nature
+          {photo.title}
         </h2>
         <div className="text-xs lg:text-sm text-black/60 mb-6">
-          #nature, #river, #ocean
+          {photo.tags.map(tag => `#${tag} `)}
         </div>
-        {/* <!-- info rows --> */}
         <div className="space-y-2.5 text-black/80 text-xs lg:text-sm">
-          {/* <!-- item start --> */}
           <div className="flex justify-between">
-            <span>Views</span>
-            <span className="font-bold">10M</span>
+            <span>{dictionary.views}</span>
+            <span className="font-bold">{photo.views}</span>
           </div>
-          {/* <!-- item ends --> */}
-          {/* <!-- item start --> */}
           <div className="flex justify-between">
-            <span>Share</span>
-            <span className="font-bold">10M</span>
+            <span>{dictionary.share}</span>
+            <span className="font-bold">{photo.share}</span>
           </div>
-          {/* <!-- item ends --> */}
-          {/* <!-- item start --> */}
           <div className="flex justify-between">
-            <span>Up loaded</span>
-            <span className="font-bold">31 January 2024</span>
+            <span>{dictionary.uploadedOn}</span>
+            <span className="font-bold">{photo.uploaded}</span>
           </div>
-          {/* <!-- item ends --> */}
         </div>
-        {/* <!-- info rows ends --> */}
 
-        {/* <!-- author info --> */}
         <div className="mt-6">
-          {/* <!-- author header --> */}
           <div className="flex justify-between items-center mb-3">
-            {/* <!--  --> */}
             <div className="flex items-center gap-3">
-              <Image className="size-12 lg:size-14 rounded-full border" src="https://i.pravatar.cc/150?Image=53" alt="avatar" />
+              <Image
+                className="size-12 lg:size-14 rounded-full border"
+                src={photo.author.avatar}
+                alt="avatar"
+                width={50}
+                height={50}
+              />
               <div className="spacy-y-3">
-                <h6 className="lg:text-lg font-bold">Saad Hasan</h6>
-                <p className="text-black/60 text-xs lg:text-sm">420 Followers</p>
+                <h6 className="lg:text-lg font-bold">{photo.author.name}</h6>
+                <p className="text-black/60 text-xs lg:text-sm">
+                  {photo.author.followers} {dictionary.followers}
+                </p>
               </div>
             </div>
-            {/* <!-- action --> */}
             <button className="flex items-center gap-1.5 text-black/60 text-xs xl:text-sm">
-              <Image src="./assets/icons/follow.svg" alt="" className="w-5 h-5" />
-              Follow
+              <img src="/follow.svg" className="w-5 h-5" alt="" />
+              {dictionary.follow}
             </button>
           </div>
-          {/* <!-- author bio --> */}
           <p className="text-xs lg:text-sm text-black/60">
-            I am a portrait photographer in Michigan, and happily share my
-            hard work so others can find and use my photos for FREE!
+            {photo.author.bio}
           </p>
         </div>
-        {/* <!-- author info ends --> */}
-        {/* <!-- actions --> */}
         <div className="mt-6">
           <div className="flex items-stretch gap-3">
-            <button
-              className="flex-1 border py-1.5 rounded text-xs lg:text-sm flex items-center justify-center text-center gap-1.5 font-bold hover:bg-yellow-400">
-              <Image src="./assets/icons/heart.svg" alt="" className="w-5 h-5" />
-              100
+            <button className="flex-1 border py-1.5 rounded text-xs lg:text-sm flex items-center justify-center text-center gap-1.5 font-bold hover:bg-yellow-400">
+              <Image
+              alt=""
+                src="/heart.svg"
+                className="w-5 h-5"
+                width={50}
+                height={50} />
+              {photo.likes}
             </button>
-            <button
-              className="flex-1 border py-1.5 rounded text-xs lg:text-sm flex items-center justify-center text-center gap-1.5 font-bold hover:bg-yellow-400">
-              <Image src="./assets/icons/save.svg" alt="" className="w-5 h-5" />
-              Save
+            <button className="flex-1 border py-1.5 rounded text-xs lg:text-sm flex items-center justify-center text-center gap-1.5 font-bold hover:bg-yellow-400">
+              <Image
+              alt=""
+                src="/save.svg"
+                className="w-5 h-5"
+                width={50}
+                height={50} />
+              {dictionary.save}
             </button>
-            <button
-              className="flex-1 border py-1.5 rounded text-xs lg:text-sm flex items-center justify-center text-center gap-1.5 font-bold hover:bg-yellow-400">
-              <Image src="./assets/icons/share.svg" alt="" className="w-5 h-5" />
-
-              Share
+            <button className="flex-1 border py-1.5 rounded text-xs lg:text-sm flex items-center justify-center text-center gap-1.5 font-bold hover:bg-yellow-400">
+              <Image
+              alt=""
+                src="/share.svg"
+                className="w-5 h-5"
+                width={50}
+                height={50} />
+              {dictionary.share}
             </button>
           </div>
         </div>
       </div>
+    </div>
   )
 }
