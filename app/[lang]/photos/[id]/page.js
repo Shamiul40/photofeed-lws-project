4c2ff33e23bb8648b36ba32cd1b74d2edbafc640
Shamiul40 <shamiul4040@gmail.com>
2026-02-10 23:57:43 +0600

    image optimized

--- app/[lang]/photos/[id]/page.js
+++ app/[lang]/photos/[id]/page.js
@@ -2,10 +2,10 @@ import PhotoDetails from '@/components/PhotoDetails'
 import React from 'react'
 
 export default function PhotosPage({params : {id, lang}}) {
-    console.log("id is",id, "lang is", lang)
+    
   return (
     <div>
-      <PhotoDetails></PhotoDetails>
+      <PhotoDetails id={id} lang={lang}></PhotoDetails>
     </div>
   )
 }
