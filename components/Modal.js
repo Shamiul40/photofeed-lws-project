b693a1015e082ae6b04fc85ee40627a3b321010b
Shamiul40 <shamiul4040@gmail.com>
2026-02-11 08:58:44 +0600

    moadal style fix

--- components/Modal.js
+++ components/Modal.js
@@ -1,5 +1,7 @@
 "use client"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
+
 import { useEffect, useRef } from "react"
 import { createPortal } from "react-dom"
 
@@ -7,6 +9,7 @@ import { createPortal } from "react-dom"
 export const Modal=({children})=>{
 
     const modalRef = useRef(null)
+    const router = useRouter()
 
     useEffect(()=>{
 
@@ -16,12 +19,28 @@ export const Modal=({children})=>{
 
     },[])
 
+    const onHide =()=>{
+        router.back()
+    }
+
     return createPortal(
-        <dialog ref={modalRef}>
-            {children}
-            <span>
-                <Image src="../public/icons/xmark.svg" alt="" width={100} height={100}/>
+        <dialog className="
+        rounded-xl
+        border border-white/10
+        bg-white/95
+        backdrop:bg-black/60
+        p-2
+        shadow-2xl
+       
+       
+        w-full
+        animate-in fade-in zoom-in-95
+      " onClose={onHide} ref={modalRef}>
+           
+            <span onClick={onHide} className="my-2 flex justify-end px-2 cursor-pointer text-sm text-zinc-500 hover:text-zinc-900 ">
+                ✕
             </span>
+             {children}
         </dialog> , document.getElementById("modalId")
     )
 }
